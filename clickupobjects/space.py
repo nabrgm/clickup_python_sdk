c25e662a80ff8b47fe09dd97bebc00880fab0437
Michael Broyles <michaelbroyles68@gmail.com>
2022-08-25 15:50:00 -0500

    get/create tags

--- clickupobjects/space.py
+++ clickupobjects/space.py
@@ -14,3 +14,19 @@ class Space(AbstractObject):
         for space in query["lists"]:
             result.append(List.create_object(data=space, target_class=List))
         return result
+
+    def get_tags(self):
+        from clickup_python_sdk.clickupobjects.tags import Tag
+
+        route = "space/" + self["id"] + "/tag"
+        query = self.api.get(route=route)
+        result = []
+        for space in query["tags"]:
+            result.append(Tag.create_object(data=space, target_class=Tag))
+        return result
+
+    def create_tag(self, name):
+        values = {"tag": {"name": name}}
+        route = "space/" + self["id"] + "/tag"
+        query = self.api.post(route=route, values=values)
+        return query
