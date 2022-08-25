c7289c0d9202e50356af05665467cb1ec0e43986
Michael Broyles <michaelbroyles68@gmail.com>
2022-08-25 15:50:28 -0500

    add tag

--- clickupobjects/task.py
+++ clickupobjects/task.py
@@ -4,3 +4,11 @@ from clickup_python_sdk.clickupobjects.abstractobject import AbstractObject
 class Task(AbstractObject):
     def __init__(self) -> None:
         super().__init__()
+
+    def add_tag(self, tag=None):
+        """
+        Args: Tag object
+        """
+        route = "task/" + self["id"] + "/tag/" + tag["name"] + "/?"
+        query = self.api.post(route=route)
+        return query
